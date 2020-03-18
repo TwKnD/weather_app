30f2f8274e2abef93b29c4ee8b58c1f9c8b10f48
David <david@pop-os.localdomain>
2020-03-18 22:01:33 +1100

    Add more info to print

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ import requests
 from config import *
 from pprint import pprint
 
+# TODO: swap request mode to XML for more data.
+
 baseUrl = "https://api.openweathermap.org/data/2.5/weather?q="
 city = 'Melbourne,AU'
-requestUrl = baseUrl + city + "&APPID=" + API_key
+requestUrl = baseUrl + city + "&units=metric" + "&APPID=" + API_key
 
 weatherData = requests.get(requestUrl).json()
 
-temp = weatherData['main']['temp']
+tempNow = weatherData['main']['temp']
+tempMax = weatherData['main']['temp_max']
+tempMin = weatherData['main']['temp_min']
+windDir = weatherData['wind']['deg']
+windSpeed = weatherData['wind']['speed']
+
 
 print("Weather For", city)
-print("Temp:", + temp)
+print("Temp:", + tempNow)
+print("Max:", + tempMax)
+print("Min:", + tempMin)
+print("Direction:", + windDir)
+print("Speed:", + windSpeed)
 print('\n')
 pprint(weatherData)
