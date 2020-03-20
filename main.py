33674447a1115c0b8867dc3128ae8378f32508ae
David <david@pop-os.localdomain>
2020-03-21 10:48:47 +1100

    XML values printing

--- main.py
+++ main.py
@@ -9,22 +9,13 @@ requestUrl = baseUrl + city + "&units=metric&mode=xml" + "&APPID=" + API_key
 weatherData = requests.get(requestUrl)
 soup = BeautifulSoup(weatherData.text, 'xml')
 
-targetTest = soup.current
-print(targetTest)
-#print(soup.prettify())
+tempNow = soup.temperature.get('value')
+tempMax = soup.temperature.get('max')
+tempMin = soup.temperature.get('min')
 
+print(tempNow)
+print(tempMax)
+print(tempMin)
 
-#tempNow =
-#tempMax = weatherData['main']['temp_max']
-#tempMin = weatherData['main']['temp_min']
-#windDir = weatherData['wind']['deg']
-#windSpeed = weatherData['wind']['speed']
-#
-#
-#print("Weather For", city)
-#print("Temp:", + tempNow)
-#print("Max:", + tempMax)
-#print("Min:", + tempMin)
-#print("Direction:", + windDir)
-#print("Speed:", + windSpeed)
-#print('\n')
+# windDir = weatherData['wind']['deg']
+# windSpeed = weatherData['wind']['speed']
