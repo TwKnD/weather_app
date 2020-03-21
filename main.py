d7784e5ab67ae474cb62d2912cf9085799e203ca
David <david@pop-os.localdomain>
2020-03-21 14:04:38 +1100

    Version 1, XML_Term

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ requestUrl = baseUrl + city + "&units=metric&mode=xml" + "&APPID=" + API_key
 weatherData = requests.get(requestUrl)
 soup = BeautifulSoup(weatherData.text, 'xml')
 
+weatherState = soup.weather.get('value').title()
 tempNow = soup.temperature.get('value')
 tempMax = soup.temperature.get('max')
 tempMin = soup.temperature.get('min')
+windInfo = soup.speed.get('name')
+windDir = soup.direction.get('code')
+windSpeed_Ms = soup.speed.get('value')
+windSpeed_Kmh = str(float(windSpeed_Ms) * 3.6)
 
-print(tempNow)
-print(tempMax)
-print(tempMin)
-
-# windDir = weatherData['wind']['deg']
-# windSpeed = weatherData['wind']['speed']
+print('\n ' + weatherState + '\n')
+print(' Now: ' + tempNow + ' C')
+print(' Max: ' + tempMax + ' C')
+print(' Min: ' + tempMin + ' C')
+print(' Wind: ' +  windSpeed_Kmh + ' Km/h ' + windDir)
