e284ff65cf10345353893fa9b72559991989df6e
David <david@pop-os.localdomain>
2020-03-21 19:39:11 +1100

    Add fg Color

--- main.py
+++ main.py
@@ -27,32 +27,35 @@ root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
-# Draw widgets
-Location_draw = tk.Label(frame, text=city)
-weatherState_draw = tk.Label(frame, text=weatherState)
-tempNow_draw = tk.Label(frame, text=tempNow)
-tempMax_draw = tk.Label(frame, text=tempMax)
-tempMin_draw = tk.Label(frame, text=tempMin)
-windInfo_draw = tk.Label(frame, text=windInfo)
-windSpeed_draw = tk.Label(frame, text=windSpeed_Kmh)
-windDir_draw = tk.Label(frame, text=windDir)
-
 # Colour widgets
 bgColor = '#32302f'
+fgColor = '#ebdbb2'
 frame.configure(bg=bgColor)
+
+# Draw widgets
+Location_draw = tk.Label(frame, text=city, bg=bgColor, fg=fgColor)
+weatherState_draw = tk.Label(frame, text=weatherState, bg=bgColor, fg=fgColor)
+tempNow_draw = tk.Label(frame, text="Current\n" + tempNow,
+                        bg=bgColor, fg=fgColor)
+tempMax_draw = tk.Label(frame, text="Max\n" + tempMax, bg=bgColor, fg=fgColor)
+tempMin_draw = tk.Label(frame, text="Min\n" + tempMin, bg=bgColor, fg=fgColor)
+windInfo_draw = tk.Label(frame, text=windInfo, bg=bgColor, fg=fgColor)
+windSpeed_draw = tk.Label(frame, text=windSpeed_Kmh + "\nKm/h",
+                          bg=bgColor, fg=fgColor)
+windDir_draw = tk.Label(frame, text=windDir, bg=bgColor, fg=fgColor)
 
 # Position widgets
 # Top
-Location_draw.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+Location_draw.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky='E')
 weatherState_draw.grid(row=0, column=3, columnspan=2, padx=10, pady=10)
 # Left
-tempNow_draw.grid(row=1, column=1, padx=10, pady=10, rowspan=2)
-tempMax_draw.grid(row=1, column=2, padx=10, pady=10)
-tempMin_draw.grid(row=2, column=2, padx=10, pady=10)
+tempNow_draw.grid(row=1, column=1, padx=10, pady=10, rowspan=2, sticky='W')
+tempMax_draw.grid(row=1, column=2, padx=10, pady=10, sticky='W')
+tempMin_draw.grid(row=2, column=2, padx=10, pady=10, sticky='W')
 # Right
 windInfo_draw.grid(row=1, column=3, columnspan=2, padx=10, pady=10)
 windSpeed_draw.grid(row=2, column=3, padx=10, pady=10)
-windDir_draw.grid(row=2, column=4, padx=10, pady=10)
+windDir_draw.grid(row=2, column=4, padx=10, pady=10, sticky='W')
 
 
 root.mainloop()
